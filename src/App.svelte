80a55f3b9db538dcbc70e9c41ca2b4a4cf58ef4e
1zumisawashun <shun.1zumisawa@gmail.com>
2022-03-18 21:43:14 +0900

    リンターとフォーマッターの追加

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,39 @@
 <script lang="ts">
-	export let name: string;
+export let name: string;
 </script>
 
 <main>
-	<h1 class="-blue">Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1 class="-blue">Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
 </main>
+<span
+  ><div>foo</div>
+  <span>bar</span></span>
 
-<style lang='scss'>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<style lang="scss">
+main {
+  text-align: center;
+  padding: 1em;
+  max-width: 240px;
+  margin: 0 auto;
+}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-		&.-blue{
-			color:  #45c496;
-		}
-	}
+h1 {
+  color: #ff3e00;
+  text-transform: uppercase;
+  font-size: 4em;
+  font-weight: 100;
+  &.-blue {
+    color: #45c496;
+  }
+}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+@media (min-width: 640px) {
+  main {
+    max-width: none;
+  }
+}
 </style>
