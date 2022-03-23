6912e977626aa05ea8c2c69b84f4ea5f0912bfd8
1zumisawashun <shun.1zumisawa@gmail.com>
2022-03-24 00:06:26 +0900

    フォームモーダルの追加

---
+++ src/components/model/shop/ShopModalForm.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { style } from '../../../middleware/style';
+import { createEventDispatcher } from 'svelte';
+import { scrollTop } from '../../../utilities/utilities';
+let dispatch = createEventDispatcher();
+let styles = { top: `${scrollTop()}px` };
+
+const handleClick = () => {
+  dispatch('click-handler');
+  dispatch('close-modal');
+};
+const closeModal = () => {
+  dispatch('close-modal');
+};
+</script>
+
+<div class="overlay" use:style="{styles}">
+  <div class="wrapper -form -medium">
+    <slot />
+    <div class="button-wrapper">
+      <button class="btn -mw150 -inverse" on:click="{handleClick}"
+        >追加する</button>
+      <button class="btn -mw150" on:click="{closeModal}">閉じる</button>
+    </div>
+  </div>
+</div>
